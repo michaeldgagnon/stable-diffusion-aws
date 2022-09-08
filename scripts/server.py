ee04df05b13d93047432548e5e16767f5198a8e1
michaeldgagnon <michaeldgagnon@gmail.com>
2022-09-07 23:17:29 -0400

    keep reading messages

--- scripts/server.py
+++ scripts/server.py
@@ -76,75 +76,76 @@ def get_query_arg(post_data, arg, default_value):
   else:
     return default_value
 
-for message in queue.receive_messages(MaxNumberOfMessages=1, WaitTimeSeconds=20):
-  try:
-    job_req = json.loads(message.body)
-    jid = job_req['id']
-    print(f'Starting: {jid}')
-    prompt = job_req['prompt']
-    W = get_query_arg(job_req, 'W', default_W)
-    H = get_query_arg(job_req, 'H', default_H)
-    C = get_query_arg(job_req, 'C', default_C)
-    scale = get_query_arg(job_req, 'scale', default_scale)
-    steps = get_query_arg(job_req, 'steps', default_steps)
-    sampler_type = get_query_arg(job_req, 'sampler', 'plms')
-    seed = get_query_arg(job_req, 'seed', 0)
-    if (seed == 0):
-      seed = random.randrange(0, np.iinfo(np.uint32).max)
-    seed_everything(seed)
+while True:
+  for message in queue.receive_messages(MaxNumberOfMessages=1, WaitTimeSeconds=20):
+    try:
+      job_req = json.loads(message.body)
+      jid = job_req['id']
+      print(f'Starting: {jid}')
+      prompt = job_req['prompt']
+      W = get_query_arg(job_req, 'W', default_W)
+      H = get_query_arg(job_req, 'H', default_H)
+      C = get_query_arg(job_req, 'C', default_C)
+      scale = get_query_arg(job_req, 'scale', default_scale)
+      steps = get_query_arg(job_req, 'steps', default_steps)
+      sampler_type = get_query_arg(job_req, 'sampler', 'plms')
+      seed = get_query_arg(job_req, 'seed', 0)
+      if (seed == 0):
+        seed = random.randrange(0, np.iinfo(np.uint32).max)
+      seed_everything(seed)
 
-    sampler = sampler_plms
-    if sampler_type == 'ddim':
-      sampler = sampler_ddim
-    elif sampler_type == 'plms':
       sampler = sampler_plms
-    elif sampler_type == 'k_dpm_2_a':
-      sampler = sampler_kdpm2_a
-    elif sampler_type == 'k_dpm_2':
-      sampler = sampler_kdpm2
-    elif sampler_type == 'k_euler_a':
-      sampler = sampler_keuler_a
-    elif sampler_type == 'k_euler':
-      sampler = sampler_keuler
-    elif sampler_type == 'k_heun':
-      sampler = sampler_kheun
-    elif sampler_type == 'k_lms':
-      sampler = sampler_klms
-      
-    data = [n_samples * [prompt]]
-    with torch.no_grad():
-        with precision_scope("cuda"):
-            with model.ema_scope():
-                for prompts in tqdm(data, desc="data"):
-                    uc = None
-                    if scale != 1.0:
-                        uc = model.get_learned_conditioning(n_samples * [""])
-                    if isinstance(prompts, tuple):
-                        prompts = list(prompts)
-                    c = model.get_learned_conditioning(prompts)
-                    shape = [C, H // f, W // f]
-                    samples_ddim, _ = sampler.sample(S=steps,
-                                                      conditioning=c,
-                                                      batch_size=n_samples,
-                                                      shape=shape,
-                                                      verbose=False,
-                                                      unconditional_guidance_scale=scale,
-                                                      unconditional_conditioning=uc,
-                                                      eta=ddim_eta,
-                                                      x_T=None)
-                    x_samples_ddim = model.decode_first_stage(samples_ddim)
-                    x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
-                    x_samples_ddim = x_samples_ddim.cpu().permute(0, 2, 3, 1).numpy()
-                    x_checked_image_torch = torch.from_numpy(x_samples_ddim).permute(0, 3, 1, 2)
-                    x_sample = x_checked_image_torch[0]
-                    x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
-                    image = Image.fromarray(x_sample.astype(np.uint8))
-                    img_file = io.BytesIO()
-                    image.save(img_file, format='PNG')
-                    s3_object = s3.Object(s3_bucket, f'sd/{jid}.png')
-                    s3_object.put(Body=img_file.getvalue(), ContentType='image/png')
-    message.delete()
-  finally:
-    torch.cuda.empty_cache()
-    torch.cuda.ipc_collect()
-    print(f'Completed: {jid}')
+      if sampler_type == 'ddim':
+        sampler = sampler_ddim
+      elif sampler_type == 'plms':
+        sampler = sampler_plms
+      elif sampler_type == 'k_dpm_2_a':
+        sampler = sampler_kdpm2_a
+      elif sampler_type == 'k_dpm_2':
+        sampler = sampler_kdpm2
+      elif sampler_type == 'k_euler_a':
+        sampler = sampler_keuler_a
+      elif sampler_type == 'k_euler':
+        sampler = sampler_keuler
+      elif sampler_type == 'k_heun':
+        sampler = sampler_kheun
+      elif sampler_type == 'k_lms':
+        sampler = sampler_klms
+        
+      data = [n_samples * [prompt]]
+      with torch.no_grad():
+          with precision_scope("cuda"):
+              with model.ema_scope():
+                  for prompts in tqdm(data, desc="data"):
+                      uc = None
+                      if scale != 1.0:
+                          uc = model.get_learned_conditioning(n_samples * [""])
+                      if isinstance(prompts, tuple):
+                          prompts = list(prompts)
+                      c = model.get_learned_conditioning(prompts)
+                      shape = [C, H // f, W // f]
+                      samples_ddim, _ = sampler.sample(S=steps,
+                                                        conditioning=c,
+                                                        batch_size=n_samples,
+                                                        shape=shape,
+                                                        verbose=False,
+                                                        unconditional_guidance_scale=scale,
+                                                        unconditional_conditioning=uc,
+                                                        eta=ddim_eta,
+                                                        x_T=None)
+                      x_samples_ddim = model.decode_first_stage(samples_ddim)
+                      x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
+                      x_samples_ddim = x_samples_ddim.cpu().permute(0, 2, 3, 1).numpy()
+                      x_checked_image_torch = torch.from_numpy(x_samples_ddim).permute(0, 3, 1, 2)
+                      x_sample = x_checked_image_torch[0]
+                      x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
+                      image = Image.fromarray(x_sample.astype(np.uint8))
+                      img_file = io.BytesIO()
+                      image.save(img_file, format='PNG')
+                      s3_object = s3.Object(s3_bucket, f'sd/{jid}.png')
+                      s3_object.put(Body=img_file.getvalue(), ContentType='image/png')
+      message.delete()
+    finally:
+      torch.cuda.empty_cache()
+      torch.cuda.ipc_collect()
+      print(f'Completed: {jid}')
